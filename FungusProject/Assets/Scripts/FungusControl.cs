dcd72b8fdf03720e94dae4dbb1ced671e8a9c0d0
Jonas_QD <richmondjonas@gmail.com>
2020-08-03 21:33:49 -0700

    Installed Fungus

---
+++ FungusProject/Assets/Scripts/FungusControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusControl : MonoBehaviour
+{
+    public Flowchart MyFlowchart; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        var blocks = MyFlowchart.GetComponents<Block>();
+        foreach (Block block in blocks) {
+            Debug.Log(block.CommandList.Count);
+            //block.CommandList[0].BroadcastMessage("Hi");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
