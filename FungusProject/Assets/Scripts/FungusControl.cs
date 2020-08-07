6c5f7a3aab09e7d5107c2c47d2643c7721932e5f
Jonas_QD <richmondjonas@gmail.com>
2020-08-06 19:30:52 -0700

    explored say functions

--- FungusProject/Assets/Scripts/FungusControl.cs
+++ FungusProject/Assets/Scripts/FungusControl.cs
@@ -5,20 +5,24 @@ using Fungus;
 
 public class FungusControl : MonoBehaviour
 {
-    public Flowchart MyFlowchart; 
+    public Flowchart MyFlowchart;
     // Start is called before the first frame update
+    public ControlAudio MyAudio;
     void Start()
     {
         var blocks = MyFlowchart.GetComponents<Block>();
+
         foreach (Block block in blocks) {
             Debug.Log(block.CommandList.Count);
+
             //block.CommandList[0].BroadcastMessage("Hi");
         }
+
     }
 
+
+
+
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
